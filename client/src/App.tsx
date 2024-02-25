f89203a8b4e6669c775024f16325bf7ff98523f4
rifqimfahmi <rifqi416@gmail.com>
2024-02-25 16:38:48 +0800

    use env variable

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {GoogleLogin, GoogleOAuthProvider, TokenResponse, useGoogleLogin} from "@react-oauth/google";
+// import dotenv from 'dotenv'
+
+function GoogleButton() {
+
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => console.log(codeResponse),
+    onError: (error) => console.log('Login Failed:', error)
+  });
+
+  return (
+    <div>
+      <GoogleLogin onSuccess={() => {}}></GoogleLogin>
+      <button onClick={() => login()}>Login</button>
+    </div>
+  )
+}
 
 function App() {
+  // dotenv.config()
+  let googleClientId: string = process.env.GOOGLE_CLIENT_ID || (() => { throw new Error("GOOGLE_CLIENT_ID env not found"); })();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -19,6 +38,12 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <p>Hello</p>
+        <GoogleOAuthProvider clientId={googleClientId}>
+          <GoogleButton></GoogleButton>
+        </GoogleOAuthProvider>
+      </div>
     </div>
   );
 }
